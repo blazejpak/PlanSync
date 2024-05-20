662ee3b1853a6cdf93a792ff9f6cf11f600088ab
blazejpak <blazej.pak@gmail.com>
2024-05-20 22:43:28 +0200

    styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.scss";
+import "./main.scss";
+
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./routes/Home/Home";
 
