7a5ada1dc3755f3dced23061025bd43e78581c1e
blazejpak <blazej.pak@gmail.com>
2024-06-19 20:09:53 +0200

    add task form

--- src/routes/Board/AddTask.tsx
+++ src/routes/Board/AddTask.tsx
@@ -1,14 +1,27 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import styles from "./AddTask.module.scss";
 import { ModalContext } from "../../context/ModalStates";
 import useClickOutside from "../../components/helpers/helpers/useClickOutside";
-import { FieldArray, Formik } from "formik";
+import { Field, FieldArray, Formik } from "formik";
 import SaveButton from "../../components/helpers/ui/SaveButton";
-import { Stack, TextField } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
 import * as yup from "yup";
+import { DateTime } from "luxon";
+import { CgClose } from "react-icons/cg";
 
 const AddTask = () => {
+  const time = DateTime.now().setLocale("en-GB").toISO().slice(0, 10);
+
   const modalRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [subtaskError, setSubtaskError] = useState(false);
 
   const { setNewTaskModal } = useContext(ModalContext);
 
@@ -27,14 +40,26 @@ const AddTask = () => {
     ),
   });
 
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div className={styles["modal-overlay"]}>
       <div className={styles["modal-content"]} ref={modalRef}>
+        <strong>Add new task</strong>
         <Formik
           initialValues={{
             task: "",
             description: "",
             subtasks: [{ subtask: "" }],
+            type: "todo",
           }}
           validationSchema={validationSchema}
           onSubmit={(values, { setSubmitting, resetForm }) => {
@@ -63,6 +88,15 @@ const AddTask = () => {
                   onChange={handleChange}
                   error={touched.task && Boolean(errors.task)}
                   helperText={touched.task && errors.task}
+                  inputProps={{ style: { fontSize: 16 } }}
+                  InputLabelProps={{
+                    style: {
+                      fontSize: 14,
+                      width: "4.5rem",
+                      backgroundColor: "white",
+                    },
+                  }}
+                  required
                 />
 
                 <TextField
@@ -75,6 +109,14 @@ const AddTask = () => {
                   onChange={handleChange}
                   error={touched.description && Boolean(errors.description)}
                   helperText={touched.description && errors.description}
+                  inputProps={{ style: { fontSize: 16 } }}
+                  InputLabelProps={{
+                    style: {
+                      fontSize: 14,
+                      width: "8rem",
+                      backgroundColor: "white",
+                    },
+                  }}
                 />
               </Stack>
 
@@ -82,13 +124,19 @@ const AddTask = () => {
                 <FieldArray
                   name="subtasks"
                   render={({ push, remove }) => (
-                    <div>
+                    <div className={styles.subtasks}>
                       {values.subtasks.length > 0 &&
                         values.subtasks.map((subtask, index) => (
-                          <div key={index}>
+                          <div className={styles.subtask} key={index}>
                             <TextField
+                              name={`subtasks.${index}.subtask`}
                               label={`Subtask ${index + 1}`}
                               type="text"
+                              value={subtask.subtask}
+                              onChange={(e) => {
+                                handleChange(e);
+                                setSubtaskError(false);
+                              }}
                               style={{ flex: 1 }}
                               error={Boolean(
                                 errors.subtasks?.[index]?.subtask &&
@@ -100,20 +148,72 @@ const AddTask = () => {
                                   ? errors.subtasks[index].subtask
                                   : ""
                               }
+                              InputLabelProps={{
+                                style: {
+                                  fontSize: 14,
+                                  width: "8rem",
+                                  backgroundColor: "white",
+                                },
+                              }}
                             />
 
-                            <button type="button" onClick={() => remove(index)}>
-                              Delete subtask
+                            <button
+                              className={styles.subtask__remove}
+                              type="button"
+                              onClick={() => remove(index)}
+                            >
+                              <CgClose size={24} />
                             </button>
                           </div>
                         ))}
+                      {subtaskError && <p>Previous subtask can't be empty</p>}
                       <button
+                        className={styles.subtask__add}
                         type="button"
-                        onClick={() => push({ subtask: "" })}
+                        onClick={() => {
+                          if (
+                            !values.subtasks[values.subtasks.length - 1].subtask
+                          ) {
+                            setSubtaskError(true);
+                          }
+                          if (
+                            values.subtasks[values.subtasks.length - 1].subtask
+                          ) {
+                            push({ subtask: "" });
+                          }
+                        }}
                       >
                         Add subtask
                       </button>
                     </div>
+                  )}
+                />
+              </Stack>
+
+              <Stack>
+                <FieldArray
+                  name="types"
+                  render={(helpers) => (
+                    <FormControl fullWidth>
+                      <InputLabel id="type">Status</InputLabel>
+
+                      <Select
+                        label="Status"
+                        id="type"
+                        name="type"
+                        value={values.type}
+                        onChange={(e) => {
+                          handleChange(e);
+                        }}
+                        open={open}
+                        onClose={handleClose}
+                        onOpen={handleOpen}
+                      >
+                        <MenuItem value="todo">Todo</MenuItem>
+                        <MenuItem value="progress">In progress</MenuItem>
+                        <MenuItem value="done">Done</MenuItem>
+                      </Select>
+                    </FormControl>
                   )}
                 />
               </Stack>
@@ -124,79 +224,6 @@ const AddTask = () => {
             </form>
           )}
         </Formik>
-
-        {/* <form onSubmit={formik.handleSubmit}>
-          <Stack spacing={2} alignItems="center">
-            <TextField
-              style={{ width: "40rem" }}
-              id="task"
-              name="task"
-              label="Task"
-              type="text"
-              value={formik.values.task}
-              onChange={formik.handleChange}
-              error={formik.touched.task && Boolean(formik.errors.task)}
-              helperText={formik.touched.task && formik.errors.task}
-            />
-
-            <TextField
-              style={{ width: "40rem" }}
-              id="description"
-              name="description"
-              label="Description"
-              type="text"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-              error={
-                formik.touched.description && Boolean(formik.errors.description)
-              }
-              helperText={
-                formik.touched.description && formik.errors.description
-              }
-            />
-          </Stack>
-
-          <FormikProvider value={formik}>
-            <FieldArray
-              name="subtasks"
-              render={({ insert, push, remove }) => (
-                <div>
-                  {formik.values.subtasks.length > 0 &&
-                    formik.values.subtasks.map((subtask, index) => (
-                      <div key={index}>
-                        <TextField
-                          label={`Subtask ${index + 1}`}
-                          type="text"
-                          style={{ flex: 1 }}
-                          error={Boolean(
-                            formik.errors.subtasks?.[index]?.subtask &&
-                              formik.touched.subtasks?.[index]?.subtask
-                          )}
-                          helperText={
-                            formik.errors.subtasks?.[index]?.subtask &&
-                            formik.touched.subtasks?.[index]?.subtask
-                              ? formik.errors.subtasks[index].subtask
-                              : ""
-                          }
-                        />
-
-                        <button type="button" onClick={() => remove(index)}>
-                          -
-                        </button>
-                      </div>
-                    ))}
-                  <button type="button" onClick={() => push({ subtask: "" })}>
-                    Add subtask
-                  </button>
-                </div>
-              )}
-            />
-          </FormikProvider>
-
-          <SaveButton type="submit" style={{ marginTop: "1.6rem" }}>
-            Add task
-          </SaveButton>
-        </form> */}
       </div>
     </div>
   );
