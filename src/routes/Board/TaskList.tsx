ee1b88dd64e909aa37b247242166e9c3a077606f
mkokosinski <mkokosinski91@gmail.com>
2024-07-19 18:38:49 +0200

    code

--- src/routes/Board/TaskList.tsx
+++ src/routes/Board/TaskList.tsx
@@ -2,15 +2,7 @@ import { useEffect, useState } from "react";
 import TaskMobile from "./mobile/TaskMobile";
 import TasksDesktop from "./desktop/TasksDesktop";
 
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { getCurrentDay } from "../../store/reducers/calendar";
-import { DateTime, Interval } from "luxon";
-import { dailyData, getAllData } from "../../store/reducers/tasks";
-
 const TaskList = () => {
-  const dispatch = useAppDispatch();
-  const day = useAppSelector(getCurrentDay);
-  const getData = useAppSelector(getAllData);
 
   const [isMobile, setIsMobile] = useState(false);
 
@@ -25,20 +17,7 @@ const TaskList = () => {
     return () => window.removeEventListener("resize", handlerResize);
   }, []);
 
-  useEffect(() => {
-    if (getData.length > 0) {
-      const newData = getData.filter((item) => {
-        const dateFrom = DateTime.fromISO(item.rangeDateFrom).startOf("day");
-        const dateTo = DateTime.fromISO(item.rangeDateTo).endOf("day");
-        const interval = Interval.fromDateTimes(dateFrom, dateTo);
-
-        return interval.contains(DateTime.fromISO(day).startOf("day"));
-      });
-      dispatch(dailyData(newData));
-    }
-  }, [getData, day]);
-
-  return <>{isMobile ? <TaskMobile /> : <TasksDesktop />}</>;
+  return isMobile ? <TaskMobile /> : <TasksDesktop />;
 };
 
 export default TaskList;
--- src/store/reducers/calendar.ts
+++ src/store/reducers/calendar.ts
@@ -1,6 +1,7 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
-import { DateTime } from "luxon";
+import { DateTime, Interval } from "luxon";
+import { dailyData } from "./tasks";
 
 const time = DateTime.now().setLocale("en-GB").toISO().slice(0, 10);
 
@@ -14,26 +15,44 @@ const initialState: InitialStateProps = {
   rangeDate: { from: time, to: time },
 };
 
+export const pickCurrentDay = createAsyncThunk(
+  "calendar/pickCurrentDay",
+  async (dataISO: string, { dispatch, getState }) => {
+    const day = (getState() as RootState).calendar.currentDay;
+    const newData = (getState() as RootState).tasks.data.filter((item) => {
+      const dateFrom = DateTime.fromISO(item.rangeDateFrom).startOf("day");
+      const dateTo = DateTime.fromISO(item.rangeDateTo).endOf("day");
+      const interval = Interval.fromDateTimes(dateFrom, dateTo);
+
+      return interval.contains(DateTime.fromISO(day).startOf("day"));
+    });
+
+    dispatch(dailyData(newData));
+
+
+    return dataISO;
+  }
+);
+
 export const calendarSlice = createSlice({
   name: "calendar",
   initialState: initialState,
   reducers: {
-    pickCurrentDay: (state, action: PayloadAction<string>) => {
-      state.currentDay = action.payload;
-    },
-
-    pickRangeDate: (
+   pickRangeDate: (
       state,
       action: PayloadAction<{ from: string; to: string }>
     ) => {
       state.rangeDate = action.payload;
     },
   },
+  extraReducers: (builder) => builder.addCase(pickCurrentDay.fulfilled, (state, action) => {
+    state.currentDay = action.payload;
+  })
 });
 
 export const getCurrentDay = (state: RootState) => state.calendar.currentDay;
 export const getRangeDate = (state: RootState) => state.calendar.rangeDate;
 
-export const { pickCurrentDay, pickRangeDate } = calendarSlice.actions;
+export const { pickRangeDate } = calendarSlice.actions;
 
 export default calendarSlice.reducer;
